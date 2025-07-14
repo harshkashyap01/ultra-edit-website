302d964d7ab3525c307e0b7915d747a6b4ddbfad
harsh kashyap <harsh.kashyap3108@gmail.com>
2025-07-14 18:22:46 +0530

    adding a python file

---
+++ 2nd.py
@@ -0,0 +1,11 @@
+
+a=int(input("enter number 1:"))
+b=int(input("enternumber2:"))
+print("a is less then b is :",a<b)#print("the squar is  :",a**2)
+name= "harsh"
+nameshort= name[1:3]
+print(nameshort)
+
+
+
+
